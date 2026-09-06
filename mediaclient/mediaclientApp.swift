6603633a5a3e18d11c4f21f4675fcf32d77e8493
JonathanEveillard <jonaeve50@gmail.com>
2026-09-06 13:37:58 -0400

    Initial Commit

---
+++ mediaclient/mediaclientApp.swift
@@ -0,0 +1,17 @@
+//
+//  mediaclientApp.swift
+//  mediaclient
+//
+//  Created by Jonathan Eveillard on 2026-09-06.
+//
+
+import SwiftUI
+
+@main
+struct mediaclientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
