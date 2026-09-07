f65da7195004f6f531a9b85f5b055f0a56231a94
JonathanEveillard <jonaeve50@gmail.com>
2026-09-07 03:30:36 -0400

    feat: ...

--- mediaclient/Models/LibrarySection.swift
+++ mediaclient/Models/LibrarySection.swift
@@ -5,3 +5,14 @@
 //  Created by Jonathan Eveillard on 2026-09-06.
 //
 
+enum ItemType{
+    case movie
+    case show
+    case unknown
+}
+
+struct LibrarySection {
+    let id: String
+    let name: String
+    let type: ItemType
+}
--- mediaclient/Models/MediaItem.swift
+++ mediaclient/Models/MediaItem.swift
@@ -4,4 +4,10 @@
 //
 //  Created by Jonathan Eveillard on 2026-09-06.
 //
+import Foundation
 
+struct MediaItem {
+    let id: String
+    let name: String
+    let imageURL: URL?
+}
